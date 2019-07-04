ee5f920da7d1d3f52a9e652d317ba2287e6ecde9
sarafina <sarafinanyawira@gmail.com>
2019-07-04 09:08:32 +0300

    Simple While Loop Example

---
+++ src/whileloop/WhileLoop.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whileloop;
+
+/**
+ *
+ * @author nyawira
+ */
+public class WhileLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int count;
+        count = 1;
+        while(count<=10){
+        System.out.println("Happy Coding");
+        count ++;
+        }
+    }
+    
+}
