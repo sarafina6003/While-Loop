b25f2f30da69f0f15bb0ee6cebc69421a23a2c15
sarafina <sarafinanyawira@gmail.com>
2019-07-04 09:51:20 +0300

    While Loop Three Different ways of executing

--- src/whileloop/WhileLoop.java
+++ src/whileloop/WhileLoop.java
@@ -15,11 +15,26 @@ public class WhileLoop {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       //code to output string for a given number of times
+       //int count;
+       //count = 1;
+       //while(count<=10){
+       //System.out.println("Happy Coding");
+       //count ++; 
+        
+       //code to output 1 to 10
+       //int count;
+       //count = 1;
+       //while(count<=10){
+       //System.out.println(count);
+       //count ++;
+        
+        //code to output 7 to 1
         int count;
-        count = 1;
-        while(count<=10){
-        System.out.println("Happy Coding");
-        count ++;
+        count=7;
+         while(count>=1){
+              System.out.println(count);
+              count--;
         }
     }
     
